980e42d1390cab22e79a7a58a8d5efe5544e04ee
telcotech01-alt <telcotech01@gmail.com>
2026-06-05 23:57:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>আমার লিংক</h1><a href="এখানে_তোমার_লিংক_বসাও">এখানে ক্লিক করো</a>'
+    return '<h1>আমার লিংক</h1><a href="https://facebook.com">আমার Facebook পেজ</a>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
